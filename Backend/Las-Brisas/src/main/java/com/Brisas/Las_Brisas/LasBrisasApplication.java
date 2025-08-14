165f03cc53863b0067ec4724fad60a01d2e2e0c0
Carol <carolmarentes31@gmail.com>
2025-08-14 14:14:10 -0500

    Creacion de la carpeta de BackEnd-JAVA

---
+++ Backend/Las-Brisas/src/main/java/com/Brisas/Las_Brisas/LasBrisasApplication.java
@@ -0,0 +1,13 @@
+package com.Brisas.Las_Brisas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LasBrisasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LasBrisasApplication.class, args);
+	}
+
+}
