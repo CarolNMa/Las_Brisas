10e00fa4f3af4d163159a6c5d29185d81e820be5
Carol <carolmarentes31@gmail.com>
2025-08-14 17:40:49 -0500

    diseño de login en react

---
+++ frontWeb/src/app/log/page.js
@@ -0,0 +1,61 @@
+"use client";
+
+import "./style.css";
+import { useState } from "react";
+
+export default function Log() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("Email:", email);
+        console.log("Password:", password);
+    };
+
+    return (
+        <div className="login-container">
+            <div className="logo">
+            </div>
+
+            <form className="login-form" onSubmit={handleSubmit}>
+                <div className="titulos">
+                    <label>Email</label>
+                </div>
+
+                <div className="input-container">
+                    <span className="icon-email"></span>
+                    <input  className="inputs"
+                        type="email"
+                        placeholder="Ingresa tu email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                    />
+                </div>
+
+                <label className="titulos">Contraseña</label>
+                <div className="input-container">
+                    <span className="icon-password"></span>
+                    <input className="inputs"
+                        type="password"
+                        placeholder="Ingresa tu contraseña"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                    />
+                </div>
+
+                <button type="submit" className="btn-login">
+                    Iniciar Sesión
+                </button>
+
+                <div className="links">
+                    <a href="#">¿Olvidaste tu contraseña?</a>
+                    <br></br>
+                    <a href="#">Crear cuenta</a>
+                </div>
+            </form>
+        </div>
+    );
+}
